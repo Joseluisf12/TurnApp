90a002b709a9daf89786a90c4ae1fee51b03ce91
Joseluisf12 <joseluisf12@hotmail.com>
2025-10-18 21:10:58 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE = 'turnapp-static-v1';
-const FILES = [
-  '/',
-  '/index.html',
-  '/app.css',
-  '/app.js',
-  '/manifest.json',
-  '/logo.svg'
-];
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("turnapp-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./app.css",
+        "./logo.svg",
+        "./manual.html"
+      ]);
+    })
+  );
+});
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
-self.addEventListener('activate', evt => {
-  evt.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', evt => {
-  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
-});
+
