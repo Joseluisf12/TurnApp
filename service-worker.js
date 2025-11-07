c25b7822b779ebf8c7150c722ddc7b644daae4f7
Joseluisf12 <joseluisf12@hotmail.com>
2025-11-07 14:34:25 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,28 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("turnapp-cache").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
         "./app.css",
+        "./app.js",
         "./logo.svg",
         "./manual.html"
       ]);
     })
   );
+
+  // Activar la versión nueva al instante
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("activate", (event) => {
+  // Tomar control inmediato de todas las pestañas abiertas
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
-// --- Forzar actualización inmediata ---
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => clients.claim());
