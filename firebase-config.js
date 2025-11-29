f830f2a61e4db1cf357b05e513e57b24a15f5dab
Joseluisf12 <joseluisf12@hotmail.com>
2025-11-29 14:57:07 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Tu configuración de Firebase para TurnApp
+// Este objeto contiene las "llaves" para conectar tu app con tu backend.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUzvvQscSUAaILinS5UuE3Rj1EtcOWcZo",
+  authDomain: "turnapp-7c9a7.firebaseapp.com",
+  projectId: "turnapp-7c9a7",
+  storageBucket: "turnapp-7c9a7.appspot.com",
+  messagingSenderId: "20052099688",
+  appId: "1:20052099688:web:12d38b38b5caf0882fb1dd"
+};
