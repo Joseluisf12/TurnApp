93aef25e470a871a3f0764829a7150f8bbb95641
Joseluisf12 <joseluisf12@hotmail.com>
2025-12-09 15:48:31 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
-    const firebaseConfig = {
-    apiKey: "AIzaSyD7nJlEjrImBXIhuF5y9tTvmkzOR9A5SRY",
-    authDomain: "turnapp12.firebaseapp.com",
-    projectId: "turnapp12",
-    storageBucket: "turnapp12.appspot.com",
-    messagingSenderId: "252416935066",
-    appId: "1:252416935066:web:ef06f33dd0d5db40388b23"
-  };
-
-
+   const firebaseConfig = {
+  apiKey: "AIzaSyD7nJlEjrImBXIhuF5y9tTvmkzOR9A5SRY",
+  authDomain: "turnapp12.firebaseapp.com",
+  projectId: "turnapp12",
+  storageBucket: "turnapp12.firebasestorage.app",
+  messagingSenderId: "252416935066",
+  appId: "1:252416935066:web:ef06f33dd0d5db40388b23"
+};
 
 
 
